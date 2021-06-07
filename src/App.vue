2f041875061fdaadb296392c4ab35d066e627c69
unknown <lesythanh063@gmail.com>
2021-06-07 21:26:54 +0700

    add header themovie

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,97 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="app">
+  <!-- Header -->
+ <div class="header">
+    <div class="header-left">
+   <div class="icon">
+     <img src="./assets/tmdb.svg" alt="tmdb"
+      height="20px"
+      width="154"
+     >
+   </div>
+      <div class="dropdow">Movies</div>
+      <div class="dropdow">TV Shows</div>
+      <div class="dropdow">People</div>
+      <div class="dropdow">More</div>
+   </div>
+   <div class="header-right">
+     <div class="item">EN</div>
+     <div class="item">Login</div>
+     <div class="item">Join TMDb</div>
+   </div>
+ </div>
+ <!-- List -->
+ <h2>Popular Movies</h2>
+<div class="container">
+  <list
+  v-for="(card,index) in cards"
+  :key="index"
+  :card="card" 
+  :index="index"
+  />
+</div>
+</div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
+ <script>
+ import List from './components/List.vue'
+  export default {
   name: 'App',
+  data(){
+    return {
+      cards:[
+        {
+        title: 'Cruella',
+        src: require('./assets/movie.jpg'),
+        },
+        {
+        title: 'Cruella',
+        src: require('./assets/movie1.jpeg'),
+        }
+      ]
+    }
+  },
   components: {
-    HelloWorld
+    List
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/* Header */
+*{
+  box-sizing: border-box;
+  font-size: 15px;
+  font-family: "Source Sans Pro", Arial, sans-serif;
+  font-weight: bold;
 }
+.header{
+  display: flex;
+  justify-content: space-between;
+  color: white;
+  background:#032541;
+  height: 64px;
+  width: 100%;
+}
+.header-left, .header-right{
+  display: flex;
+  align-items: center;
+}
+.dropdow{
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  padding: 1rem;
+}
+.item{
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  padding: 1rem;
+}
+.icon{
+  padding: 2rem;
+  cursor: pointer;
+}
+/* List */
+
 </style>
---
+++ src/components/List.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="list">
+    <div class="list-image">
+        <img :src="card.src" />
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'List',
+    props: ['card', 'index']
+}
+</script>
+
+<style>
+
+</style>
